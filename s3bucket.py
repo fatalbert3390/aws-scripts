c904d2353af56bb3c3e99a3eba86eac6dfadbe8c
fatalbert3390 <fatalbert3390@gmail.com>
2017-11-10 20:24:15 -0500

    created file with boto3 cmd

---
+++ s3bucket.py
@@ -0,0 +1,7 @@
+# script designed to utilize boto3 commands to create an s3 bucket
+# by reading a list of domains and piping each line into the "create bucket"
+# command and creating each bucket individually
+
+import boto3
+
+s3.create_bucket(Bucket='mybucket')

f3c69d792b4dd9d1666d61d9fb491c403074be6c
alex <fatalbert3390@gmail.com>
2017-11-13 07:23:34 -0500

    updated bucket add cmd to work

--- s3bucket.py
+++ s3bucket.py
@@ -2,10 +2,16 @@
 # by reading a list of domains and piping each line into the "create bucket"
 # command and creating each bucket individually
 
-import boto3
+#import boto3
 
-s3 = boto3.client('s3')
-l = open('testbucketlist.txt')
+#s3 = boto3.client('s3')
+#l = open('testbucketlist.txt')
 
-for line in l
-	s3.create_bucket(line, '')
+#for line in l
+#	s3.create_bucket(line, '')
+
+with open('testbucketlist.txt') as in_file:
+    for bucket_name in in_file:
+        bucket = bucket_name.replace('\n', '')
+        print("Creating: {}".format(bucket))
+        s3.create_bucket(Bucket=bucket)

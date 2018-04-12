0eb1aaa1236dc18f9eb4401bd1fadd764dfd3eaf
fatalbert3390 <fatalbert3390@gmail.com>
2018-04-12 09:31:19 -0400

    pushed lambda snapshot funcs

---
+++ lambda-snapshot-delete.py
@@ -0,0 +1,54 @@
+# Delete snapshots older than retention period
+
+import boto3
+from botocore.exceptions import ClientError
+
+from datetime import datetime,timedelta
+
+#def delete_snapshot(snapshot_id):
+#    print "Deleting snapshot %s " % (snapshot_id)
+#    try:  
+#        ec2resource = boto3.resource('ec2')
+#        snapshot = ec2resource.Snapshot(snapshot_id)
+#        snapshot.delete()
+#    except ClientError as e:
+#        print "Caught exception: %s" % e
+        
+#    return
+    
+def lambda_handler(event, context):
+    
+    # Get current timestamp in UTC
+    now = datetime.now()
+
+	# AWS Account ID    
+	account_id = '041619730184'
+    
+    # Define retention period in days
+    retention_days = 7
+    
+    # Create EC2 client
+    ec2 = boto3.client('ec2')
+    
+    # Filtering by snapshot timestamp comparison is not supported
+    # grab all snapshots for specified instances:
+    result = ec2.describe_snapshots( VolumeIds=[
+	'vol-06edc3097410fe5d4', # Triton C (TESTING)
+        #'vol-00bcbc49', # Adonis D
+        #'vol-b7f5e8fb', # Athena D
+	]
+    )
+    
+    for snapshot in result['Snapshots']:
+        print "Checking snapshot %s which was created on %s" % (snapshot['SnapshotId'],snapshot['StartTime'])
+    
+        # Remove timezone info from snapshot in order for comparison to work below
+        snapshot_time = snapshot['StartTime'].replace(tzinfo=None)
+    
+        # Subtract snapshot time from now returns a timedelta
+        # Check if the timedelta is greater than retention days
+        if (now - snapshot_time) > timedelta(retention_days):
+            print "Snapshot is older than configured retention of %d days" % (retention_days)
+            delete_snapshot(snapshot['SnapshotId'])
+        else:
+            print "Snapshot is newer than configured retention of %d days so we keep it" % (retention_days)

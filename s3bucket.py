e0cfd9e508fa1d57361821b7384690c38cc50e5a
fatalbert3390 <fatalbert3390@gmail.com>
2018-04-12 09:50:39 -0400

    simplified s3bucket script

--- s3bucket.py
+++ s3bucket.py
@@ -1,27 +1,23 @@
 # script designed to utilize boto3 commands to create an s3 bucket
+# and push a simple web config to it
 # by reading a list of domains and piping each line into the "create bucket"
-# command and creating each bucket individually
 
 import boto3
 
 s3 = boto3.client('s3')
 
-with open('testbucketlist.txt') as in_file:
-    for bucket_name in in_file:
-        bucket = bucket_name.replace('\n', '')
-        print("Creating: {}".format(bucket))
-        s3.create_bucket(Bucket=bucket)
-
-# create website configuration
-website_configuration = {
+web_config = {
     'RedirectAllRequestsTo': {
 	'HostName': 'second-to-none.com',
 	'Protocol': 'http',
     },
 }
 
-# apply the policy to the bucket
-s3.put_bucket_website(
-    Bucket=bucket,
-    WebsiteConfiguration=website_configuration
-)
+with open('testbucketlist.txt') as in_file:
+    for bucket_name in in_file:
+        bucket = bucket_name.replace('\n', '')
+        print("Creating: {}".format(bucket))
+        s3.create_bucket(Bucket=bucket)
+        s3.put_bucket_website(
+            Bucket=bucket,
+            WebsiteConfiguration=web_config

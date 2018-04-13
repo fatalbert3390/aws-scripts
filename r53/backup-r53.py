cbb18364dcfe4e657b10b409f30eceb279c2a7b9
fatalbert3390 <fatalbert3390@gmail.com>
2018-04-13 15:04:10 -0400

    testing find func in script

--- r53/backup-r53.py
+++ r53/backup-r53.py
@@ -4,4 +4,18 @@ client = boto3.client("route53")
 
 zones_output = client.list_hosted_zones()
 
-print zones_output
+# 's' is the string you're trying to parse. 
+# 'first' is the beginning character
+# 'last' is the end character
+
+def find_between(s, first, last):
+    try:
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
+
+print find_between(str(zones_output), "e/", "',")
+
+

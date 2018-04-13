7d0cf2b4595d6def35cb87780f9697a38d77fb8d
fatalbert3390 <fatalbert3390@gmail.com>
2018-04-13 09:52:30 -0400

    added finder script and sample txt

---
+++ find_zoneID.py
@@ -0,0 +1,19 @@
+
+# 's' is the string you're trying to parse. 
+# 'first' is the beginning character
+# 'last' is the end character
+
+
+test_str = "u'Id': '/hostedzone/ZJ50IDU48RG9U',"
+
+def find_between(s, first, last):
+    try:
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
+
+print find_between(test_str, "e/", "',")
+
+

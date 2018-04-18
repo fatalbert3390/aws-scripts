5a928cc2f0ca616e95156718ad5d3409eaa3b77c
fatalbert3390 <fatalbert3390@gmail.com>
2018-04-18 14:55:44 -0400

    removed erroneous code

--- r53/backup-r53.py
+++ r53/backup-r53.py
@@ -10,24 +10,6 @@ json = client.list_hosted_zones()
 hosted_zones = json['HostedZones']
 
 regexp = re.compile("/hostedzone/(.*)$")
-#zone_ids = []
 
 for hosted_zone in hosted_zones:
-	#zone_ids.append
 	print(regexp.search(hosted_zone['Id']).group(1))
-
-#print(zone_ids) # this should have all that you need :)
-
-# 's' is the string you're trying to parse.
-# 'first' is the beginning character
-# 'last' is the end character
-
-#def find_between(s, first, last):
-#    try:
-#        start = s.index(first) + len(first)
-#        end = s.index(last, start)
-#        return s[start:end]
-#    except ValueError:
-#        return ""
-
-#print find_between(str(zones_output), "e/", "',")

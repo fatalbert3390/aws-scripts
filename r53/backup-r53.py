d5bcc88f85db479b8feb2589475630b66ed41eef
fatalbert3390 <fatalbert3390@gmail.com>
2018-04-20 08:15:35 -0400

    modified for loop to print all rr's for HZ's and store to variable

--- r53/backup-r53.py
+++ r53/backup-r53.py
@@ -1,10 +1,10 @@
 import boto3
 import re
 
-client = boto3.client("route53")
-
+r53_client = boto3.client('route53')
+s3_client = boto3.client('s3')
 # json would be your reponse to play with
-json = client.list_hosted_zones()
+json = r53_client.list_hosted_zones()
 
 # hosted_zones is just well...your hosted zones
 hosted_zones = json['HostedZones']
@@ -12,6 +12,7 @@ hosted_zones = json['HostedZones']
 regexp = re.compile("/hostedzone/(.*)$")
 
 for hosted_zone in hosted_zones:
-    client.list_resource_record_sets(HostedZoneId=(
-	    (regexp.search(hosted_zone['Id']).group(1))
-	)
+    zone_id = regexp.search(hosted_zone['Id']).group(1)
+    print r53_client.list_resource_record_sets(HostedZoneId=zone_id)
+
+s3_client.put_object(Bucket='STN_Route53')

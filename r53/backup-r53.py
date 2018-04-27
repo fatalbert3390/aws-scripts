a5032a3f3f8902501400249683241e647661f17a
fatalbert3390 <fatalbert3390@gmail.com>
2018-04-27 16:08:42 -0400

    added empty list, append response to list, and json parser

--- r53/backup-r53.py
+++ r53/backup-r53.py
@@ -1,12 +1,13 @@
 import boto3
 import re
 import time
+import json
 
 r53_client = boto3.client('route53')
 s3_client = boto3.client('s3')
 timestamp = time.strftime("%Y-%m-%d @ %H:%M:%S")
 
-# json would be your reponse to play with
+# hz_json would be your reponse to play with
 hz_json = r53_client.list_hosted_zones()
 
 # hosted_zones is just well...your hosted zones
@@ -14,11 +15,14 @@ hosted_zones = hz_json['HostedZones']
 
 regexp = re.compile("/hostedzone/(.*)$")
 
+r53_backup = []
+
 for hosted_zone in hosted_zones:
     zone_id = regexp.search(hosted_zone['Id']).group(1)
-    backup_json = r53_client.list_resource_record_sets(HostedZoneId=zone_id)
+    r53_backup.append(r53_client.list_resource_record_sets(HostedZoneId=zone_id))
     s3_client.put_object(
-        Body=b'{}'.format(backup_json),
+        Body=(json.dumps(r53_backup, indent=4, sort_keys=False)),
         Bucket='alex-stn-testing',
         Key='r53backup ' + timestamp
     )
+#print(json.dumps(r53_backup, indent=4, sort_keys=False))

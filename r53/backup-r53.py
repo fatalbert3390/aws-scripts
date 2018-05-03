fc8f8619d62f605450db476f116d74b5d6ad3843
Alex Smith <fatalbert3390@gmail.com>
2018-05-03 10:17:09 -0400

    Added lambda handler and indents

--- r53/backup-r53.py
+++ r53/backup-r53.py
@@ -3,6 +3,18 @@ import re
 import time
 import json
 
+def lambda_handler(event, context):
+    r53_client = boto3.client('route53')
+    s3_client = boto3.client('s3')
+    timestamp = time.strftime("%Y-%m-%d @ %H:%M:%S")
+
+    # hz_json would be your reponse to play with
+    hz_json = r53_client.list_hosted_zones()
+
+    # hosted_zones is just well...your hosted zones
+    hosted_zones = hz_json['HostedZones']
+
+    regexp = re.compile("/hostedzone/(.*)$")
 r53_client = boto3.client('route53')
 s3_client = boto3.client('s3')
 timestamp = time.strftime("%Y-%m-%d @ %H:%M:%S")

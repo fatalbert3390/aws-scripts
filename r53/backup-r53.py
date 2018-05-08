97e4c6f77a9f54cb9016e55801b4e910e7be6b95
Alex Smith <fatalbert3390@gmail.com>
2018-05-08 10:16:37 -0400

    removed erroneously copied lines

--- r53/backup-r53.py
+++ r53/backup-r53.py
@@ -15,26 +15,15 @@ def lambda_handler(event, context):
     hosted_zones = hz_json['HostedZones']
 
     regexp = re.compile("/hostedzone/(.*)$")
-r53_client = boto3.client('route53')
-s3_client = boto3.client('s3')
-timestamp = time.strftime("%Y-%m-%d @ %H:%M:%S")
 
-# hz_json would be your reponse to play with
-hz_json = r53_client.list_hosted_zones()
+    r53_backup = []
 
-# hosted_zones is just well...your hosted zones
-hosted_zones = hz_json['HostedZones']
-
-regexp = re.compile("/hostedzone/(.*)$")
-
-r53_backup = []
-
-for hosted_zone in hosted_zones:
-    zone_id = regexp.search(hosted_zone['Id']).group(1)
-    r53_backup.append(r53_client.list_resource_record_sets(HostedZoneId=zone_id))
-    s3_client.put_object(
-        Body=(json.dumps(r53_backup, indent=4, sort_keys=False)),
-        Bucket='alex-stn-testing',
-        Key='r53backup ' + timestamp
-    )
-#print(json.dumps(r53_backup, indent=4, sort_keys=False))
+    for hosted_zone in hosted_zones:
+        zone_id = regexp.search(hosted_zone['Id']).group(1)
+        r53_backup.append(r53_client.list_resource_record_sets(HostedZoneId=zone_id))
+        s3_client.put_object(
+            Body=(json.dumps(r53_backup, indent=4, sort_keys=False)),
+            Bucket='alex-stn-testing',
+            Key='r53backup ' + timestamp
+        )
+    #print(json.dumps(r53_backup, indent=4, sort_keys=False))

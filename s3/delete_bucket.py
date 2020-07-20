dd47b4fd31daa406c55c18d88d499462b95cc335
Alex Smith <fatalbert3390@gmail.com>
2020-07-20 12:46:51 -0400

    added bucket removal

---
+++ s3/delete_bucket.py
@@ -0,0 +1,15 @@
+import boto3
+from pprint import pprint
+
+s3 = boto3.client('s3')
+
+bucket_names = []
+
+bucket_list = s3.list_buckets()
+
+for buckets in bucket_list['Buckets']:
+    bucket_names.append(buckets['Name'])
+
+for bucket in bucket_names:
+    s3.delete_bucket(Bucket=bucket)
+    print("Deleting bucket {}".format(bucket))

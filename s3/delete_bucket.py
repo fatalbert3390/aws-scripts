51729782477fc53430d43079974bbe0d0fb29391
Alex Smith <fatalbert3390@gmail.com>
2020-07-21 08:55:25 -0400

    updated

--- s3/delete_bucket.py
+++ s3/delete_bucket.py
@@ -9,7 +9,6 @@ bucket_list = s3.list_buckets()
 
 for buckets in bucket_list['Buckets']:
     bucket_names.append(buckets['Name'])
-
-for bucket in bucket_names:
-    s3.delete_bucket(Bucket=bucket)
-    print("Deleting bucket {}".format(bucket))
+    for bucket in bucket_names:
+        s3.delete_bucket(Bucket=bucket)
+        print("Deleting bucket {}".format(bucket))

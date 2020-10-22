0729567bdb6fc5c096ecde7e4e8b3153964c5ddb
Alex Smith <fatalbert3390@gmail.com>
2020-10-22 16:37:45 -0400

    moved variables

--- ec2/start_instance.py
+++ ec2/start_instance.py
@@ -1,8 +1,10 @@
 # starts instance specified by instance ID
+
 import boto3
 
+ec2 = boto3.client('ec2')
+
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
 
     ec2.start_instances(
         InstanceIds=[
--- ec2/stop_instance.py
+++ ec2/stop_instance.py
@@ -1,9 +1,10 @@
 # stops instance specified by instance ID
+
 import boto3
 
-def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
+ec2 = boto3.client('ec2')
 
+def lambda_handler(event, context):
     ec2.stop_instances(
         InstanceIds=[
             'i-########',
